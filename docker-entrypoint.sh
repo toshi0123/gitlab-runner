26cd05839419e4b4b1a5ec271661a29c0b7e5824
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-11-29 14:39:04 +0900

    Deleted runner install which fails

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,4 @@
 
 /usr/bin/dockerd -p /run/dockerd.pid &
 
-gitlab-ci-multi-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
-
 exec gitlab-ci-multi-runner run --user=gitlab-runner --working-directory=/home/gitlab-runner
