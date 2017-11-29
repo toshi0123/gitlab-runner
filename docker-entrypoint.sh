4c5d1b38b7b88fb2f54f64a5ee390a2c986060b3
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-11-29 15:02:54 +0900

    Fixed failed to run runner

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,6 @@
 
 /usr/bin/dockerd -p /run/dockerd.pid &
 
+gitlab-ci-multi-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+
 exec gitlab-ci-multi-runner run --user=gitlab-runner --working-directory=/home/gitlab-runner
