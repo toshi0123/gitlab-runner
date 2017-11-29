9369e3b872336975cec7ce380419cbbc427fa661
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-11-29 15:12:43 +0900

    Added options of gitlab-ci-multi-runner

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,4 @@
 
 gitlab-ci-multi-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
 
-exec gitlab-ci-multi-runner run --user=gitlab-runner --working-directory=/home/gitlab-runner
+exec gitlab-ci-multi-runner run --user=gitlab-runner --working-directory=/home/gitlab-runner "$@"
