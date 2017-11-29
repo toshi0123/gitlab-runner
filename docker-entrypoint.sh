4215e389aa582b056101a9aaa2539430208dd1da
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-11-29 15:28:16 +0900

    Added hosts check for own registry

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-/usr/bin/dockerd -p /run/dockerd.pid --insecure-registry myregistry:5000 &
+DOCKER_OPT=""
+grep "myregistry" /etc/hosts -q && DOCKER_OPT="--insecure-registry myregistry:5000"
+
+/usr/bin/dockerd -p /run/dockerd.pid ${DOCKER_OPT} &
 
 gitlab-ci-multi-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
 
