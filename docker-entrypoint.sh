fe40e8d8b0db24feace61140aec2918a12044182
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-11-29 14:23:47 +0900

    Added to run dockerd

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+/usr/bin/dockerd -p /run/dockerd.pid &
+
 gitlab-ci-multi-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
 
 exec gitlab-ci-multi-runner run --user=gitlab-runner --working-directory=/home/gitlab-runner
