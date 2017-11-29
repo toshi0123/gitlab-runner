79c3a3f8fb588261a3dad06063f763f3e1120c06
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-11-29 15:23:40 +0900

    Added insecure registry

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/usr/bin/dockerd -p /run/dockerd.pid &
+/usr/bin/dockerd -p /run/dockerd.pid --insecure-registry myregistry:5000 &
 
 gitlab-ci-multi-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
 
