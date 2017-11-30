57001273132ee4456512ffbaed0179baa7766f36
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-11-30 14:44:36 +0900

    Changed to gitlab-runner

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ grep "myregistry" /etc/hosts -q && DOCKER_OPT="--insecure-registry myregistry:50
 /usr/bin/dockerd -p /run/dockerd.pid ${DOCKER_OPT} &
 
 if [ ! -e /etc/gitlab-runner/config.toml ];then
-gitlab-ci-multi-runner register -n || exit 1
+gitlab-runner register -n || exit 1
 fi
 
-exec gitlab-ci-multi-runner run --user=gitlab-runner --working-directory=/home/gitlab-runner "$@"
+exec gitlab-runner run --user=gitlab-runner --working-directory=/home/gitlab-runner "$@"
